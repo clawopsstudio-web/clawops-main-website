75a43863e4a4c20520acb04488502990fd665f03
root <root@vmi3094584.contaboserver.net>
2026-04-09 00:49:21 +0200

    feat: update pricing to Ampere.sh style with VPS specs and value comparison

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,6 +1,6 @@
 import Navbar from "@/components/ui/Navbar";
 import Footer from "@/components/sections/Footer";
-import PricingPageClient from "./PricingPageClient";
+import AmpereStylePricing from "./AmpereStylePricing";
 
 export default function PricingPage() {
   return (
